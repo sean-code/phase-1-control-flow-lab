5676badc4d3064e31efbbadb0a973db4c65defea
sean-code <john.nganga@student.moringaschool.com>
2022-08-15 21:52:53 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//First Two Tests
+const ride = 199;
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return 'This one is on me!';
+  }else if (ride > 2000 && ride < 2500){        //AND
+    return 'I will gladly take your thirty bucks.'
+  }
+else {
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+// Ternary Part
+function ternaryCheckCity(city){
+  return city === 'NYC'? "Ok, sounds good." : "No go."
+}
+
+
+// Switch Case Statement
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return "Thank you so much.";
+  }
+  else if (tip === 'not as generous'){
+    return "Thank you.";
+  }else{
+    return "Bye."
+  }
 }
